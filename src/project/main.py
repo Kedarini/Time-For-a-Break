0eaf2444c100ef46bceab459fab91e0075554b59
Kedarini <kedardamps@gmail.com>
2026-04-07 19:16:32 +0200

    Add styling and alignmenets

--- src/project/main.py
+++ src/project/main.py
@@ -1,72 +1,112 @@
-from sqlite3.dbapi2 import connect
 import sys
-from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QVBoxLayout, QSpacerItem, QSizePolicy
 from PySide6.QtCore import QTimer, Qt, Slot
 
+
 class Program(QWidget):
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-		self.duration = 5 # 5 minutes
+        self.duration = 1  # 5 minutes
 
-		self.setWindowTitle("Time For a Break")
-		self.setStyleSheet("""
-			background-color: #89986D;
-			color: #F6F0D7;
-			font-size: 40px;
-			""")
+        self.setWindowTitle("Time For a Break")
+        self.setStyleSheet("""
+            QWidget {
+                background-color: #2E3A2F;
+                color: #F6F0D7;
+                font-family: Segoe UI, Arial;
+            }
 
-		self.label = QLabel(
-			"It is time to take a break!",
-			alignment=Qt.AlignCenter
-		)
-		self.label.show()
+            QLabel#title {
+                font-size: 48px;
+                font-weight: bold;
+            }
 
-		self.countdown = QLabel(
-			f"{self.duration // 60} min {self.duration % 60} sec",
-			alignment = Qt.AlignCenter
-		)
+            QLabel#timer {
+                font-size: 64px;
+                font-weight: 600;
+                color: #E8E2C6;
+            }
 
-		self.button = QPushButton("Break done!")
-		self.button.setStyleSheet("""
-			width: 10vw
-			""")
-		self.button.clicked.connect(lambda: self.quit())
+            QPushButton {
+                background-color: #9CAB84;
+                color: black;
+                border-radius: 12px;
+                padding: 15px 40px;
+                font-size: 20px;
+            }
 
-		self.layout = QVBoxLayout()
-		self.layout.addWidget(self.label)
-		self.layout.addWidget(self.countdown)
-		self.setLayout(self.layout)
+            QPushButton:hover {
+                background-color: #B5C3A1;
+            }
 
-		self.timer = QTimer()
-		self.timer.timeout.connect(self.timer_update)
-		self.timer.start(1000)
+            QPushButton:pressed {
+                background-color: #7E8C6A;
+            }
+        """)
 
-	def timer_update(self):
-		self.duration -= 1
-		if self.duration <= 0:
-			self.timer.stop()
-			self.layout.addWidget(self.button)
-		else:
-			self.countdown.setText(
-				f"{self.duration // 60} min {self.duration % 60} sec"
-			)
+        # Title
+        self.label = QLabel("Time to take a break")
+        self.label.setObjectName("title")
+        self.label.setAlignment(Qt.AlignCenter)
 
+        # Timer
+        self.countdown = QLabel(self.format_time())
+        self.countdown.setObjectName("timer")
+        self.countdown.setAlignment(Qt.AlignCenter)
 
-	@Slot()
-	def quit(self):
-		for widget in QApplication.topLevelWidgets():
-			widget.close()
+        # Button
+        self.button = QPushButton("Break done")
+        self.button.clicked.connect(self.quit)
+        self.button.hide()
+
+        # Layout
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        self.layout.addSpacerItem(QSpacerItem(20, 100, QSizePolicy.Minimum, QSizePolicy.Expanding))
+        self.layout.addWidget(self.label, alignment=Qt.AlignCenter)
+        self.layout.addSpacing(20)
+        self.layout.addWidget(self.countdown, alignment=Qt.AlignCenter)
+        self.layout.addSpacing(40)
+        self.layout.addWidget(self.button, alignment=Qt.AlignCenter)
+        self.layout.addSpacerItem(QSpacerItem(20, 100, QSizePolicy.Minimum, QSizePolicy.Expanding))
+
+        self.setLayout(self.layout)
+
+        # Timer logic
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer_update)
+        self.timer.start(1000)
+
+    def format_time(self):
+        minutes = self.duration // 60
+        seconds = self.duration % 60
+        return f"{minutes:02}:{seconds:02}"
+
+    def timer_update(self):
+        self.duration -= 1
+        if self.duration <= 0:
+            self.timer.stop()
+            self.countdown.setText("00:00")
+            self.label.setText("Break finished")
+            self.button.show()
+        else:
+            self.countdown.setText(self.format_time())
+
+    @Slot()
+    def quit(self):
+        QApplication.quit()
 
 
 if __name__ == "__main__":
-	app = QApplication([])
+    app = QApplication([])
 
-	widgets = []
-	for screen in app.screens():
-		widget = Program()
-		widget.setGeometry(screen.geometry())
-		widget.showFullScreen()
-		widgets.append(widget)
+    widgets = []
+    for screen in app.screens():
+        widget = Program()
+        widget.setGeometry(screen.geometry())
+        widget.showFullScreen()
+        widgets.append(widget)
 
-	sys.exit(app.exec())
+    sys.exit(app.exec())
